e1be0c3b9b0e6c71fb609a7d4a3148ac7de4f75a
TrivenaPanjaitan <trivena.panjaitan@ist.id>
2024-07-26 10:07:42 +0700

    init

---
+++ src/components/MovieList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1>Popular Movies</h1>
+    <div v-if="movies.length === 0">Loading...</div>
+    <div v-else class="movies">
+      <div v-for="movie in movies" :key="movie.id" class="movie">
+        <img :src="getImageUrl(movie.poster_path)" alt="Movie Poster" />
+        <h3>{{ movie.title }}</h3>
+        <button @click="generateBarcode(movie.id)">Generate Barcode</button>
+        <qrcode-stream v-if="selectedMovie === movie.id" @decode="onDecode"></qrcode-stream>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { QrcodeStream } from 'vue-qrcode-reader';
+
+export default {
+  components: {
+    QrcodeStream,
+  },
+  data() {
+    return {
+      movies: [],
+      selectedMovie: null,
+    };
+  },
+  methods: {
+    async fetchMovies() {
+      const config = {
+        headers: {
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZjY0ZjkyMzQ3MGMyODA1OWE5OTUzYjM2ZWEzYzlmZSIsIm5iZiI6MTcxOTM2OTUxNy40NTA4MzMsInN1YiI6IjY2NmNmNDdmZjUyYzE3YTNkMjk5OTE1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GEpGSY1XrpbSfIZbyBui_cffKOiIn4m8eqUPoCv4y_4',
+        },
+      };
+      try {
+        const response = await axios.get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', config);
+        this.movies = response.data.results;
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    },
+    getImageUrl(path) {
+      return `https://image.tmdb.org/t/p/w500${path}`;
+    },
+    generateBarcode(movieId) {
+      this.selectedMovie = movieId;
+    },
+    onDecode(decodedString) {
+      alert(`Scanned Ticket: ${decodedString}`);
+      this.selectedMovie = null;
+    },
+  },
+  created() {
+    this.fetchMovies();
+  },
+};
+</script>
+
+<style scoped>
+.movies {
+  display: flex;
+  flex-wrap: wrap;
+}
+.movie {
+  margin: 10px;
+}
+</style>
